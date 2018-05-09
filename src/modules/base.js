6e33523615a69da9a40c7926e76a827a0b9a7389
meesrutten <mees.rutten@cobrasystems.nl>
2018-05-09 12:09:13 +0200

    Add objects, remove hi.txt file, add systemfont

--- src/modules/base.js
+++ src/modules/base.js
@@ -1,12 +1,16 @@
 
 const scene = new THREE.Scene(),
+	sizes = {
+		width: window.innerWidth / 2,
+		height: window.innerHeight / 2,
+	},
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ),
 	renderer = new THREE.WebGLRenderer(),
 	controls = new THREE.OrbitControls( camera )
 
 camera.position.z = 20
 
-renderer.setSize( window.innerWidth, window.innerHeight )
+renderer.setSize( sizes.width, sizes.height )
 renderer.setPixelRatio( window.devicePixelRatio )
 document.body.appendChild( renderer.domElement )
 
@@ -20,7 +24,7 @@ scene.add( hemisphereLight )
 
 function resize() {
 
-	renderer.setSize( window.innerWidth, window.innerHeight )
+	renderer.setSize( sizes.width, sizes.height )
 	renderer.setPixelRatio( window.devicePixelRatio )
 
 	camera.aspect = window.innerWidth / window.innerHeight
