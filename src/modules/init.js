530555df0b3a3a13fea6d76e5f787cdbd6445c42
servinlp <servin.nissen@gmail.com>
2018-05-09 12:36:35 +0200

    Object load script with first object

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,4 +1,5 @@
 
+import loadObject from './load-object.js'
 import render from './render.js'
 import { renderer, scene } from './base.js'
 
@@ -6,6 +7,18 @@ function init() {
 
 	const axesHelper = new THREE.AxesHelper( 5 )
 	scene.add( axesHelper )
+
+	loadObject( 'main-tank' )
+		.then( object => {
+
+			scene.add( object )
+
+		} )
+		.catch( err => {
+
+			console.error( err )
+
+		} )
 
 	renderer.animate( render )
 
